6e6116334f423141c63fb4aab06b3368a40d79f3
banrj <eh.ehr@bk.ru>
2022-12-25 21:35:20 +0200

    string

---
+++ kbk6.2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Богомолов Даниил 13 вариант базовый
+
+Console.WriteLine("Введите текст: ");
+string text = Console.ReadLine();
+string[] words = text.Split(' ');
+
+for (int index = 1; index < words.Length; index++)
+{
+    Console.Write(words[index]+ ' ');
+
+}    
